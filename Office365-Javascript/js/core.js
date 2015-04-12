81774832b140d5ba5244eb83608eff007a165270
Jomit <jomitv@live.com>
2015-04-11 23:29:48 -0700

    added the initial structure
    added the initial structure for authenticating and fetching documents
    from Office 365

---
+++ Office365-Javascript/js/core.js
@@ -0,0 +1,61 @@
+﻿
+var O365JS = O365JS || {};
+
+O365JS.core = (function () {
+    "use strict";
+    var clientId = '<client id here>...';
+    var replyUrl = '<reply url here>...';
+
+    function callService(options) {
+        return $.Deferred(function (defer) {
+            var type = "GET";
+            if (options.type) {
+                type = options.type;
+            }
+            $.ajax({
+                url: options.apiUrl,
+                type: type,
+                dataType: "json",
+                contentType: "application/json",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Authorization", "Bearer " + options.accessToken);
+                }
+            }).done(function (data) {
+                defer.resolve(data);
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                alert(jqXHR.responseText);
+            });
+        }).promise();
+    }
+
+    function authenticate(apiUrl) {
+        var apiURI = URI(apiUrl);
+        var url = 'https://login.windows.net/common/oauth2/authorize?' +
+                   "response_type=" + encodeURI('token') + "&" +
+                   "client_id=" + encodeURI(clientId) + "&" +
+                   "resource=" + apiURI.protocol() + "://" + apiURI.authority() + "&" +
+                   "redirect_uri=" + encodeURI(replyUrl);
+
+        window.location = url;
+    }   
+
+    function getAccessTokenFromUrl() {
+        var name = "access_token";
+        var query = window.location.hash.substr(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0].toLowerCase() == name.toLowerCase()) { return pair[1]; }
+        }
+        return null;
+    }
+
+    return {
+        callService: callService,
+        authenticate: authenticate,
+        getAccessTokenFromUrl: getAccessTokenFromUrl
+    }
+})();
+
+
+
---
+++ Office365-Javascript/js/indexViewModel.js
@@ -0,0 +1,52 @@
+﻿/// <reference path="core.js" />
+/// <reference path="lib/knockout-3.3.0.js" />
+/// <reference path="lib/jquery-2.1.3.min.js" />
+
+(function (O365JS) {
+    "use strict";
+    O365JS.indexViewModel = (function () {
+        var accessToken = ko.observable();
+        var myDocuments = ko.observableArray();
+        var apiUrl = 'https://devfun-my.sharepoint.com/_api/v1.0/me/files';
+
+        function initialize() {
+            var token = O365JS.core.getAccessTokenFromUrl();
+            if (token != null) {
+                accessToken(token);
+            }
+        }
+
+        function authenticate() {
+            var token = O365JS.core.getAccessTokenFromUrl();
+            if (token == null) {
+                O365JS.core.authenticate(apiUrl);
+            }
+            else {
+                accessToken(token);
+            }
+        }
+
+        function getDocuments() {
+            O365JS.core.callService({ apiUrl: apiUrl , accessToken: accessToken()})
+                .then(function (data) {
+                    var files = ko.utils.arrayFilter(data.value, function (docItem) {
+                        return docItem.type == "File";
+                    });
+                    myDocuments(files);
+                });
+        }
+
+        return {
+            initialize: initialize,
+            authenticate: authenticate,
+            accessToken: accessToken,
+            myDocuments: myDocuments,
+            getDocuments: getDocuments
+        }
+    })();
+    var indexView = document.getElementById("indexViewModel");
+    if (indexView !== null) {
+        O365JS.indexViewModel.initialize();
+        ko.applyBindings(O365JS.indexViewModel, indexView);
+    }
+})(O365JS);
